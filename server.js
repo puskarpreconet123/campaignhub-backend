ff2c6ef7ff443c0ac18c44acfbf020cf8a984d00
puskarpreconet123 <puskar.preconet@gmail.com>
2026-02-11 13:07:39 +0530

    update port

--- server.js
+++ server.js
@@ -18,9 +18,11 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/campaign", campaignRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
   console.log("MongoDB connected");
-  app.listen(5000, () => console.log("Server running on port 5000"));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
 .catch(err => console.log("error while conecting mongoose", err));
